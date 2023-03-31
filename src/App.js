38901564a91ee164f39f975c0fee1e68f2a679c2
haskey225 <kouamefranck06@gmail.com>
2023-03-31 15:40:07 +0000

    Error datas

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import { UserContext } from './Application/Context/UserContextProvider';
 import './app.css';
 import { logo } from './Application/Image';
 // import { getInvoices } from './Application/api/pay';
 import { apiTest } from './Application/api/data';
+import { datas } from './Application/api/data';
 
 const initState = {
   name: "",
@@ -14,9 +15,12 @@ const initState = {
 
 function App(props) {
 
-  const [state, setState] = React.useState(initState)
 
-    
+  const [state, setState] = useState(initState);
+  const [show, setShow] = useState('none');
+  const [rentSelected, setSrentSelected]= useState('n1');
+
+
   const handleChange = e => {
     const { name, value } = e.target
     setState({ ...state, [name]: value })
@@ -28,6 +32,23 @@ function App(props) {
     if (parseInt(state.amounte) > 1000) {
       // getInvoices(state.amounte); //Pour les paimement
     }
+
+  }
+
+  // Hidden rent content
+  const handleCheckBox = (e) => {
+    // console.log(e.target.value);
+    if (e.target.name === 'invoice') {
+
+      setShow(e.target.value);
+    }
+
+    if (e.target.name === 'select') {
+
+      console.log(e.target.name);
+    }
+
+    console.log(datas)
 
   }
 
@@ -43,12 +64,41 @@ function App(props) {
         <hr />
         <div className="account-type">
           <h4>Je paie :</h4>
-          <input type="radio" value="none" id="radioOne" name="account" defaultChecked />
+          <input type="radio" ive='frere' value="none" id="radioOne" name="invoice" defaultChecked onChange={handleCheckBox} />
           <label htmlFor="radioOne" className="radio">Une facture</label>
-          <input type="radio" value="none" id="radioTwo" name="account" />
+          <input type="radio" ive='gfrere' value="block" id="radioTwo" name="invoice" onChange={handleCheckBox} />
           <label htmlFor="radioTwo" className="radio">Mon loyer</label>
         </div>
         <hr />
+        <div className='account-type' style={{ display: show }}>
+          <h4>Selectionnez votre numéro !</h4>
+          <label id="icon" htmlFor="select"><i className="fas fa-user-magnifying-glass"></i></label>
+          <select name='select' onChange={handleCheckBox} style={{
+            width: '300px',
+            height: '38px',
+            borderRadius: '0 5px 5px 0'
+          }}>
+
+            <option value="N1">{datas.N1['code']}</option>
+            <option value="N2">{datas.N2['code']}</option>
+            <option value="N3">{datas.N3['code']}</option>
+            <option value="N4">{datas.N4['code']}</option>
+            <option value="N5">{datas.N5['code']}</option>
+            <option value="N7">{datas.N7['code']}</option>
+            <option value="N8">{datas.N8['code']}</option>
+            <option value="N9">{datas.N9['code']}</option>
+            <option value="N10">{datas.N10['code']}</option>
+
+          </select>
+          <label id="icon" htmlFor="name"><i className="fas fa-user"></i></label>
+          <input type="text" readOnly name="name" id="name" placeholder="Nom complet"  />
+          <label id="icon" htmlFor="tel"><i className="fas fa-thin fa-phone"></i></label>
+          <input type="text" readOnly name="tel" id="name" placeholder="Numéro de telephone"  />
+          <label id="icon" htmlFor="amounte"><i className="fas fa-light fa-dollar-sign"></i></label>
+          <input type="text" readOnly name="amounte" id="name" placeholder="Montant"  />
+        </div>
+        
+        <hr style={{ display: show }} />
         <label id="icon" htmlFor="name"><i className="fas fa-user"></i></label>
         <input type="text" name="name" id="name" placeholder="Nom complet" required value={state.name} onChange={handleChange} />
         <label id="icon" htmlFor="tel"><i className="fas fa-thin fa-phone"></i></label>
--- src/Application/api/data.js
+++ src/Application/api/data.js
@@ -1,10 +1,85 @@
 import React from 'react';
 import axios from 'axios';
+
+export const datas = {
+    n1: {
+        "id": "1",
+        "im": "CO-01-IGR",
+        "code": "N1",
+        "loyer": "170000",
+        "occupant": "FLORA OUATTARA",
+        "status": "OC"
+    },
+    n2: {
+        "id": "2",
+        "im": "CO-01-IGR",
+        "code": "N2",
+        "loyer": "160000",
+        "occupant": "ALEX KOFFI",
+        "status": "OC"
+    },
+    n3: {
+        "id": "2",
+        "im": "CO-01-IGR",
+        "code": "N3",
+        "loyer": "160000",
+        "occupant": "ALEX KOFFI",
+        "status": "OC"
+    },
+    n4: {
+        "id": "2",
+        "im": "CO-01-IGR",
+        "code": "N4",
+        "loyer": "160000",
+        "occupant": "ALEX KOFFI",
+        "status": "OC"
+    },
+    n5: {
+        "id": "2",
+        "im": "CO-01-IGR",
+        "code": "N5",
+        "loyer": "160000",
+        "occupant": "ALEX KOFFI",
+        "status": "OC"
+    },
+    n7: {
+        "id": "2",
+        "im": "CO-01-IGR",
+        "code": "N7",
+        "loyer": "160000",
+        "occupant": "ALEX KOFFI",
+        "status": "OC"
+    },
+    n8: {
+        "id": "2",
+        "im": "CO-01-IGR",
+        "code": "N8",
+        "loyer": "160000",
+        "occupant": "ALEX KOFFI",
+        "status": "OC"
+    },
+    n9: {
+        "id": "2",
+        "im": "CO-01-IGR",
+        "code": "N9",
+        "loyer": "160000",
+        "occupant": "ALEX KOFFI",
+        "status": "OC"
+    },
+    n10: {
+        "id": "2",
+        "im": "CO-01-IGR",
+        "code": "N10",
+        "loyer": "160000",
+        "occupant": "ALEX KOFFI",
+        "status": "OC"
+    }
+}
 const config = {
-    Origin : 'http://localhost:3000',
+    Origin: 'http://localhost:3000',
     'Access-control-request-method': 'POST',
-    'Content-Type':'Application/json'
-  };
+    'Content-Type': 'Application/json'
+};
 
 export function apiTest(props) {
     const data = {
@@ -16,8 +91,8 @@ export function apiTest(props) {
         }
     }
 
-    axios.post("https://riafsarl.com/api/api.php", data, {config}).then(respo =>{
-    }).catch(reason=>{
+    axios.post("https://riafsarl.com/api/api.php", data, { config }).then(respo => {
+    }).catch(reason => {
         console.error(reason);
     });
 
